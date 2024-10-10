c94c9c1e04a06e20561561f2c5bf598bfb374b82
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2024-10-10 17:01:48 +0300

    integer types

---
+++ From_Novice_To_Ninja/control_flow.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  // Include the iostream library
+
+using namespace std;  // Use the standard namespace
+
+int main() {
+    int age = 25;
+
+    if (age >= 18) {
+        cout << "You are eligible to vote." << endl;
+    }
+    else {
+        cout << "You are not eligible to vote yet." << endl;
+    }
+
+    return 0;  // Return statement for the main function
+}
