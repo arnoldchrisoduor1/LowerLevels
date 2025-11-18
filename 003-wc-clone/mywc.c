d5c5fe386cd940a102e78547ea6ca59a069a76e8
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-11-18 19:25:33 +0300

    finished part one

--- 003-wc-clone/mywc.c
+++ 003-wc-clone/mywc.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 void count_file(FILE *fp, long *lines, long *words, long *chars) {
     int c;
     int in_word = 0;
     
-    *lines = *words = *chars = 0;  // Initialize counters
+    *lines = *words = *chars = 0;
     
     while ((c = fgetc(fp)) != EOF) {
         (*chars)++;
@@ -30,9 +31,20 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     long total_lines = 0, total_words = 0, total_chars = 0;
     int i;
+    int use_stdin = 0;
     
-    // If no arguments, read from stdin
+    // If no arguments, we'll use stdin
     if (argc == 1) {
+        use_stdin = 1;
+    }
+    
+    // Check if we have only "-" as argument
+    if (argc == 2 && strcmp(argv[1], "-") == 0) {
+        use_stdin = 1;
+        argc = 1; // Trick to make it behave like no arguments
+    }
+    
+    if (use_stdin) {
         long lines, words, chars;
         count_file(stdin, &lines, &words, &chars);
         printf("%ld %ld %ld\n", lines, words, chars);
@@ -43,24 +55,25 @@ int main(int argc, char *argv[]) {
     for (i = 1; i < argc; i++) {
         long lines, words, chars;
         
-        fp = fopen(argv[i], "r");
-        if (fp == NULL) {
-            printf("mywc: %s: No such file\n", argv[i]);
-            continue;  // Skip to next file
+        if (strcmp(argv[i], "-") == 0) {
+            count_file(stdin, &lines, &words, &chars);
+        } else {
+            fp = fopen(argv[i], "r");
+            if (fp == NULL) {
+                fprintf(stderr, "mywc: %s: No such file\n", argv[i]);
+                continue;
+            }
+            count_file(fp, &lines, &words, &chars);
+            fclose(fp);
         }
-        
-        count_file(fp, &lines, &words, &chars);
-        fclose(fp);
         
         printf("%ld %ld %ld %s\n", lines, words, chars, argv[i]);
         
-        // Add to totals
         total_lines += lines;
         total_words += words;
         total_chars += chars;
     }
     
-    // Print totals if multiple files
     if (argc > 2) {
         printf("%ld %ld %ld total\n", total_lines, total_words, total_chars);
     }
