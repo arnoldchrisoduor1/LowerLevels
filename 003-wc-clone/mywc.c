e7414e5c78cd5195865596d98715567b1ff793b0
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-11-18 16:16:16 +0300

    showing counts from a file

--- 003-wc-clone/mywc.c
+++ 003-wc-clone/mywc.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main (int argc, char *argv[])
 {
     FILE *fp;
     int c; // int because fgetc returns int.
+    long lines = 0, words = 0, chars = 0;
+    int in_word = 0;
 
     if (argc < 2) {
         printf("Usage: %s <filename\n>", argv[0]);
@@ -19,9 +22,25 @@ int main (int argc, char *argv[])
 
     // Read file character by character.
     while((c = fgetc(fp)) != EOF) {
-        putchar(c);
+        chars ++; // counting every character.
+
+        if (c == '\n') {
+            lines++;
+        }
+
+        // now for the word counting logic.
+        if (isspace(c)) {
+            in_word = 0; // to show we are in a white space not in a word
+        } else {
+            // if we were not in a word and now in a non-whitespace (first letter after whitespace)
+            if (!in_word) {
+                words++;
+                in_word = 1;
+            }
+        }
     }
 
+    printf("%ld %ld %ld %s\n", lines, words, chars, argv[1]);
     fclose(fp);
     return 0;
 
